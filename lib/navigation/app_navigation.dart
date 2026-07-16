f722ec68cd4ddb64833110a35cc8857d12c0fff7
MerriDev <271108080+MerriDev@users.noreply.github.com>
2026-07-16 21:27:16 +0500

    Practice...

--- lib/navigation/app_navigation.dart
+++ lib/navigation/app_navigation.dart
@@ -1,9 +1,26 @@
+// import 'package:flutter/material.dart';
+// import 'package:practice_app/screens/home_screen.dart';
+
+// class AppNavigation {
+//   static Future<void> handleSplashTimeout(BuildContext context) async {
+//     await Future.delayed(const Duration(seconds: 4));
+
+//     if (!context.mounted) return;
+
+//     Navigator.pushAndRemoveUntil(
+//       context,
+//       MaterialPageRoute(builder: (context) => const HomeScreen()),
+//       (Route<dynamic> route) => false,
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:practice_app/screens/home_screen.dart';
 
 class AppNavigation {
   static Future<void> handleSplashTimeout(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 7));
+    await Future.delayed(const Duration(seconds: 4));
 
     if (!context.mounted) return;
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:practice_app/widgets/custom_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(155, 43, 40, 40),
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'PracticeApp',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(155, 43, 40, 40),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome to Home Screen!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 40),
+              CustomButton(
+                text: 'Get Started',
+                onPressed: () {
+                  print('Get Started Clicked!');
+                },
+              ),
+              const SizedBox(height: 15),
+              CustomButton(
+                text: 'Log Out',
+                backgroundColor: Colors.redAccent,
+                onPressed: () {
+                  print('Log Out Clicked!');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
